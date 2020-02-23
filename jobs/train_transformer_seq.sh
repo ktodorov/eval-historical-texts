42c0aa37cb5dd81699e9e5319e11928992006279
ktodorov <kztodorov@outlook.com>
2020-02-23 14:05:05 +0100

    Modifies transformer sequence jobs

--- jobs/train_transformer_seq.sh
+++ jobs/train_transformer_seq.sh
@@ -24,7 +24,7 @@ echo copying finished
 
 cd "$TMPDIR"/eval-historical-texts
 
-srun python3 -u run.py --device cuda --seed 13 --configuration transformer-sequence --learning-rate 1e-3 --metric-types jaccard-similarity levenshtein-distance --language english --challenge ocr --batch-size 16 --hidden-dimension 512 --sentence-piece-vocabulary-size 30522 --encoder-embedding-size 128 --decoder-embedding-size 16 --dropout 0.1 --number-of-layers 3 --number-of-heads 8 --enable-external-logging --pretrained-weights bert-base-cased --max-training-minutes 4320 --include-pretrained-model --pretrained-model-size 768 --pretrained-max-length 512  --learn-encoder-embeddings --validation-dataset-reduction-size 0.020 > output/seq-bert-13.txt
+srun python3 -u run.py --device cuda --seed 13 --eval-freq 100 --configuration transformer-sequence --learning-rate 1e-3 --metric-types jaccard-similarity levenshtein-distance --language english --challenge ocr --batch-size 4 --hidden-dimension 64 --sentence-piece-vocabulary-size 30522 --dropout 0.1 --number-of-layers 2 --number-of-heads 2 --enable-external-logging --pretrained-weights bert-base-cased --max-training-minutes 4320 --include-pretrained-model --pretrained-model-size 768 --pretrained-max-length 512  --learn-encoder-embeddings --validation-dataset-reduction-size 0.1 > output/seq-bert-13.txt
 
-cp "$TMPDIR"/eval-historical-texts/wandb $HOME/eval-historical-texts/wandb
-cp "$TMPDIR"/eval-historical-texts/results $HOME/eval-historical-texts/results/from-scratch/seq-to-char-pretrained
+cp -a -n "$TMPDIR"/eval-historical-texts/wandb $HOME/eval-historical-texts/wandb
+cp -a "$TMPDIR"/eval-historical-texts/results $HOME/eval-historical-texts/results/from-scratch/tr-seq-to-char-pretrained
--- jobs/train_transformer_seq_scratch.sh
+++ jobs/train_transformer_seq_scratch.sh
@@ -24,7 +24,7 @@ echo copying finished
 
 cd "$TMPDIR"/eval-historical-texts
 
-srun python3 -u run.py --device cuda --seed 13 --eval-freq 100 --configuration transformer-sequence --learning-rate 1e-3 --metric-types jaccard-similarity levenshtein-distance --language english --challenge ocr --batch-size 16 --hidden-dimension 256 --sentence-piece-vocabulary-size 30522 --encoder-embedding-size 64 --decoder-embedding-size 16 --dropout 0 --number-of-layers 3 --number-of-heads 8 --enable-external-logging --pretrained-weights bert-base-cased --max-training-minutes 4320 --learn-encoder-embeddings --validation-dataset-reduction-size 0.020 > output/tr-seq-scratch-13.txt
+srun python3 -u run.py --device cuda --seed 13 --eval-freq 100 --configuration transformer-sequence --learning-rate 1e-3 --metric-types jaccard-similarity levenshtein-distance --language english --challenge ocr --batch-size 4 --hidden-dimension 64 --sentence-piece-vocabulary-size 30522 --dropout 0.1 --number-of-layers 2 --number-of-heads 2 --enable-external-logging --pretrained-weights bert-base-cased --max-training-minutes 4320 --learn-encoder-embeddings --validation-dataset-reduction-size 0.1 > output/tr-seq-scratch-13.txt
 
-cp "$TMPDIR"/eval-historical-texts/wandb $HOME/eval-historical-texts/wandb
-cp "$TMPDIR"/eval-historical-texts/results $HOME/eval-historical-texts/results/from-scratch/tr-seq-to-char
+cp -a -n "$TMPDIR"/eval-historical-texts/wandb $HOME/eval-historical-texts/wandb
+cp -a "$TMPDIR"/eval-historical-texts/results $HOME/eval-historical-texts/results/from-scratch/tr-seq-to-char
