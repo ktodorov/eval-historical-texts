74a5fe9d52d8b77cb1d0866e998579d4b4c5eaa5
ktodorov <kztodorov@outlook.com>
2020-03-09 11:16:23 +0100

    Updates jobs to reflect newest code changes

--- jobs/train_kbert_german_1.sh
+++ jobs/train_kbert_german_1.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=3
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=25:00:00
+#SBATCH --time=73:00:00
 #SBATCH --mem=60000M
 #SBATCH -p gpu_shared
 #SBATCH --gres=gpu:1
@@ -23,12 +23,14 @@ echo copying to SCRATCH...
 shopt -s extglob # This is to enable ! on the next line
 mkdir -p "$TMPDIR"/eval-historical-texts/results
 cp -a $HOME/eval-historical-texts/!(results) "$TMPDIR"/eval-historical-texts
+mkdir -p "$TMPDIR"/eval-historical-texts/results/semantic-change/kbert/$LANGUAGE
+cp -a $HOME/eval-historical-texts/results/semantic-change/kbert/$LANGUAGE/ "$TMPDIR"/eval-historical-texts/results/semantic-change/kbert/
 echo copying finished
 
 cd "$TMPDIR"/eval-historical-texts
 
-srun python3 -u run.py --device cuda --seed 13 --eval-freq 1000 --patience 100 --configuration kbert --learning-rate 1e-5 --language $LANGUAGE --corpus $CORPUS --challenge semantic-change --batch-size 2 --pretrained-weights bert-base-multilingual-cased --pretrained-max-length 512 --pretrained-vocabulary-size 119547 --max-training-minutes 1440 --enable-external-logging --skip-validation --checkpoint-name $CORPUS --reset-training-on-early-stop --training-reset-epoch-limit 1 --resets-limit 2 > output/semeval-$LANGUAGE-$CORPUS-13.txt
+srun python3 -u run.py --device cuda --seed 13 --eval-freq 1000 --patience 100 --configuration kbert --learning-rate 1e-5 --language $LANGUAGE --corpus $CORPUS --challenge semantic-change --batch-size 2 --pretrained-weights bert-base-multilingual-cased --pretrained-max-length 512 --pretrained-vocabulary-size 119547 --max-training-minutes 4320 --enable-external-logging --skip-validation --checkpoint-name $CORPUS --reset-training-on-early-stop --training-reset-epoch-limit 1 --resets-limit 2 > output/semeval-$LANGUAGE-$CORPUS-13.txt
 
 cp -a "$TMPDIR"/eval-historical-texts/wandb $HOME/eval-historical-texts/wandb
-cp -a "$TMPDIR"/eval-historical-texts/results $HOME/eval-historical-texts/results/from-scratch/sem-eval-$LANGUAGE-$CORPUS
+cp -a "$TMPDIR"/eval-historical-texts/results/semantic-change/kbert/$LANGUAGE $HOME/eval-historical-texts/results/semantic-change/kbert/$LANGUAGE
 cp -a "$TMPDIR"/eval-historical-texts/data/semantic-change/kbert/$LANGUAGE $HOME/eval-historical-texts/results/from-scratch/sem-eval-$LANGUAGE-$CORPUS-ids
--- jobs/train_sequence_scratch.sh
+++ jobs/train_sequence_scratch.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=3
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=72:00:00
+#SBATCH --time=73:00:00
 #SBATCH --mem=60000M
 #SBATCH -p gpu_shared
 #SBATCH --gres=gpu:1
@@ -24,7 +24,7 @@ echo copying finished
 
 cd "$TMPDIR"/eval-historical-texts
 
-srun python3 -u run.py --device cuda --seed 13 --configuration sequence-to-char --learning-rate 1e-3 --metric-types jaccard-similarity levenshtein-distance --language english --challenge ocr --batch-size 16 --hidden-dimension 256 --sentence-piece-vocabulary-size 30522 --encoder-embedding-size 64 --decoder-embedding-size 16 --dropout 0 --number-of-layers 1 --enable-external-logging --pretrained-weights bert-base-cased --max-training-minutes 4320 --learn-encoder-embeddings --validation-dataset-reduction-size 0.020 > output/seq-scratch-13.txt
+srun python3 -u run.py --device cuda --seed 13 --eval-freq 200 --configuration sequence-to-char --learning-rate 1e-4 --metric-types jaccard-similarity levenshtein-distance --language english --challenge post-ocr-correction --batch-size 256 --hidden-dimension 256 --pretrained-vocabulary-size 30522 --encoder-embedding-size 64 --decoder-embedding-size 64 --dropout 0.2 --number-of-layers 1 --enable-external-logging --pretrained-weights bert-base-cased --max-training-minutes 4320 --learn-new-embeddings --validation-dataset-limit-size 50000 > output/seq-scratch-13.txt
 
 cp "$TMPDIR"/eval-historical-texts/wandb $HOME/eval-historical-texts/wandb
 cp "$TMPDIR"/eval-historical-texts/results $HOME/eval-historical-texts/results/from-scratch/seq-to-char
