ce3c965a808ed46b912c69a44adc70ab97081ef9
ktodorov <kztodorov@outlook.com>
2020-04-11 14:51:19 +0200

    Removes comments from attention layer

--- models/rnn_encoder_decoder/sequence_attention.py
+++ models/rnn_encoder_decoder/sequence_attention.py
@@ -20,29 +20,15 @@ class SequenceAttention(nn.Module):
 
     def forward(self, hidden, encoder_context):
 
-        # hidden = [batch size, dec hid dim]
-        # encoder_context = [src len, batch size, enc hid dim * 2]
-
         batch_size = encoder_context.shape[0]
         src_len = encoder_context.shape[1]
 
-        # repeat decoder hidden state src_len times
-        # hidden = hidden.unsqueeze(1).repeat(1, src_len, 1)
-
-        # encoder_context = encoder_context.permute(1, 0, 2)
         hidden = hidden.permute(1, 0, 2)
-
-        # hidden = [batch size, src len, dec hid dim]
-        # encoder_context = [batch size, src len, enc hid dim * 2]
 
         energy = torch.tanh(
             self._attention.forward(torch.cat((hidden, encoder_context), dim=2)))
 
-        # energy = [batch size, src len, dec hid dim]
-
         attention = self.v.forward(energy).squeeze(2)
-
-        # attention= [batch size, src len]
 
         result = F.softmax(attention, dim=1)
         return result
