52ef6f4a88735ffe31236fde5e411b2011c37d9d
ktodorov <kztodorov@outlook.com>
2020-04-16 12:52:55 +0200

    Adds SGD optimizer

---
+++ optimizers/sgd_optimizer.py
@@ -0,0 +1,31 @@
+from torch import optim
+from torch.optim.optimizer import Optimizer
+from overrides import overrides
+
+from models.model_base import ModelBase
+
+from optimizers.optimizer_base import OptimizerBase
+from services.arguments.arguments_service_base import ArgumentsServiceBase
+
+
+class SGDOptimizer(OptimizerBase):
+    def __init__(
+            self,
+            arguments_service: ArgumentsServiceBase,
+            model: ModelBase):
+        super().__init__(arguments_service, model)
+
+    def _init_optimizer(self) -> Optimizer:
+        optimizer = optim.SGD(
+            self._model.parameters(),
+            lr=self._learning_rate)
+
+        return optimizer
+
+    @overrides
+    def step(self):
+        self._optimizer.step()
+
+    @overrides
+    def zero_grad(self):
+        self._optimizer.zero_grad()
