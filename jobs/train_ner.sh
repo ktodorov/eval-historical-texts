e44dd69d6244c622b8d24516f33c97ac497722da
ktodorov <kztodorov@outlook.com>
2020-04-30 10:35:55 +0200

    Fixes NER job default arguments

--- jobs/train_ner.sh
+++ jobs/train_ner.sh
@@ -27,16 +27,22 @@ cd "$TMPDIR"/eval-historical-texts
 FT=""
 if [ ! -z "$FASTTEXT" ]
 then
-    FT="ft"
+    FT="-ft"
 fi
 
 PRETR=""
 if [ ! -z "$INCLUDEPRETR" ]
 then
-    PRETR="pretr"
+    PRETR="-pretr"
 fi
 
-srun python3 -u run.py --device cuda --eval-freq 200 --seed 13 --patience 150 --epochs 3000 --configuration rnn-simple --learning-rate 1e-2 --metric-types f1-score precision-score recall-score --language $LANGUAGE --challenge named-entity-recognition --batch-size 128 --enable-external-logging --pretrained-weights $PRETRAINEDWEIGHTS --hidden-dimension 256 --embeddings-size 64 --dropout 0.5 --number-of-layers 1 --label-type $LABELTYPE --reset-training-on-early-stop --training-reset-epoch-limit 5 $INCLUDEPRETR --pretrained-model-size 768 --pretrained-max-length 512 --learn-new-embeddings --checkpoint-name $LANGUAGE-$LABELTYPE-$FT-$PRETRAINEDMODEL-$PRETR $NOATTENTION --bidirectional-rnn $FASTTEXT --fasttext-model $FASTTEXTMODEL --fasttext-model-size 300 --max-training-minutes 4300 --merge-subwords --learn-character-embeddings --character-embeddings-size 32 --replace-all-numbers --pretrained-model $PRETRAINEDMODEL
+if [ -z "$PRETRAINEDMODEL" ]
+then
+    PRETRAINEDMODEL="bert"
+fi
+
+echo "srun python3 -u run.py --device cuda --eval-freq 200 --seed 13 --patience 150 --epochs 3000 --configuration rnn-simple --learning-rate 1e-2 --metric-types f1-score precision-score recall-score --language $LANGUAGE --challenge named-entity-recognition --batch-size 128 --enable-external-logging --pretrained-weights $PRETRAINEDWEIGHTS --hidden-dimension 256 --embeddings-size 64 --dropout 0.5 --number-of-layers 1 --label-type $LABELTYPE --reset-training-on-early-stop --training-reset-epoch-limit 5 $INCLUDEPRETR --pretrained-model-size 768 --pretrained-max-length 512 --learn-new-embeddings --checkpoint-name $LANGUAGE-$LABELTYPE$FT-$PRETRAINEDMODEL$PRETR $NOATTENTION --bidirectional-rnn $FASTTEXT --fasttext-model $FASTTEXTMODEL --fasttext-model-size 300 --max-training-minutes 4300 --merge-subwords --learn-character-embeddings --character-embeddings-size 32 --replace-all-numbers --pretrained-model $PRETRAINEDMODEL"
+srun python3 -u run.py --device cuda --eval-freq 200 --seed 13 --patience 150 --epochs 3000 --configuration rnn-simple --learning-rate 1e-2 --metric-types f1-score precision-score recall-score --language $LANGUAGE --challenge named-entity-recognition --batch-size 128 --enable-external-logging --pretrained-weights $PRETRAINEDWEIGHTS --hidden-dimension 256 --embeddings-size 64 --dropout 0.5 --number-of-layers 1 --label-type $LABELTYPE --reset-training-on-early-stop --training-reset-epoch-limit 5 $INCLUDEPRETR --pretrained-model-size 768 --pretrained-max-length 512 --learn-new-embeddings --checkpoint-name $LANGUAGE-$LABELTYPE$FT-$PRETRAINEDMODEL$PRETR $NOATTENTION --bidirectional-rnn $FASTTEXT --fasttext-model $FASTTEXTMODEL --fasttext-model-size 300 --max-training-minutes 4300 --merge-subwords --learn-character-embeddings --character-embeddings-size 32 --replace-all-numbers --pretrained-model $PRETRAINEDMODEL
 
 cp -a "$TMPDIR"/eval-historical-texts/wandb/ $HOME/eval-historical-texts/
 cp -a "$TMPDIR"/eval-historical-texts/results/ $HOME/eval-historical-texts/
