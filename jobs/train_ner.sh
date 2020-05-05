752bffbc6eca3765a4f54dc155dfef605812c948
ktodorov <kztodorov@outlook.com>
2020-05-05 14:36:43 +0200

    Adds recent changes to NER job

--- jobs/train_ner.sh
+++ jobs/train_ner.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=3
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=72:00:00
+#SBATCH --time=13:00:00
 #SBATCH --mem=60000M
 #SBATCH -p gpu_shared
 #SBATCH --gres=gpu:1
@@ -54,23 +54,79 @@ then
 elif [ $ENTITYTAGS == "coarse" ]
 then
     ENTITYTAGTYPES="--entity-tag-types literal-coarse metonymic-coarse "
+elif [ $ENTITYTAGS == "1" ]
+then
+    ENTITYTAGTYPES="--entity-tag-types literal-fine "
+elif [ $ENTITYTAGS == "2" ]
+then
+    ENTITYTAGTYPES="--entity-tag-types literal-coarse "
+elif [ $ENTITYTAGS == "3" ]
+then
+    ENTITYTAGTYPES="--entity-tag-types metonymic-fine "
+elif [ $ENTITYTAGS == "4" ]
+then
+    ENTITYTAGTYPES="--entity-tag-types metonymic-coarse "
+elif [ $ENTITYTAGS == "5" ]
+then
+    ENTITYTAGTYPES="--entity-tag-types component "
+elif [ $ENTITYTAGS == "6" ]
+then
+    ENTITYTAGTYPES="--entity-tag-types nested "
 fi
 
 CHARACTEREMBEDDINGS=""
 CHARCHECKPOINT=""
-if [ ! -z "$CHARSIZE" ]
+if [ ! -z "$CHARESIZE" ]
 then
-    CHARACTEREMBEDDINGS="--learn-character-embeddings --character-embeddings-size $CHARSIZE"
-    CHARCHECKPOINT="-ce"
+    CHARACTEREMBEDDINGS="--learn-character-embeddings --character-embeddings-size $CHARESIZE --character-hidden-size $CHARHSIZE"
+    CHARCHECKPOINT="-ce$CHARESIZE-ch$CHARHSIZE"
+fi
+
+LEARNINGRATE="$LR"
+if [ -z "$LR" ]
+then
+    LEARNINGRATE="1e-4"
+fi
+
+HIDDENSIZE="$HIDDEN"
+if [ -z "$HIDDEN" ]
+then
+    HIDDENSIZE="256"
+fi
+
+EMBEDDINGSSIZE="$EMB"
+if [ -z "$EMB" ]
+then
+    EMBEDDINGSSIZE="64"
+fi
+
+NUMBERLAYERS="$LAYERS"
+if [ -z "$LAYERS" ]
+then
+    NUMBERLAYERS="1"
+fi
+
+BIDIRECTIONAL="--bidirectional-rnn"
+BIARG="-bi"
+if [ -z "$UNI" ]
+then
+    BIDIRECTIONAL=""
+    BIARG=""
+fi
+
+DROPOUT="$DR"
+if [ -z "$DR" ]
+then
+    DROPOUT="0.3"
 fi
 
 CHECKPOINTNAME=""
 if [ -z "$CHECKPOINT" ]
 then
-    CHECKPOINTNAME="$LANGUAGE-$ENTITYTAGS-$FT-$PRETRAINEDMODEL$PRETR$CHARCHECKPOINT"
+    CHECKPOINTNAME="$LANGUAGE-$ENTITYTAGS-$FT-$PRETRAINEDMODEL$PRETR$CHARCHECKPOINT-h$HIDDENSIZE-e$EMBEDDINGSSIZE-l$NUMBERLAYERS$BIARG-d$DR"
 fi
 
-srun python3 -u run.py --device cuda --eval-freq 100 --seed 13 --patience 50 --epochs 3000 --configuration rnn-simple --learning-rate 1e-2 --metric-types f1-score precision-score recall-score --language $LANGUAGE --challenge named-entity-recognition --batch-size 128 --enable-external-logging --pretrained-weights $PRETRAINEDWEIGHTS --hidden-dimension 512 --embeddings-size 128 --dropout 0.3 --number-of-layers 1 $ENTITYTAGTYPES --reset-training-on-early-stop --training-reset-epoch-limit 5 $INCLUDEPRETR --pretrained-model-size 768 --pretrained-max-length 512 --learn-new-embeddings --checkpoint-name $CHECKPOINTNAME --no-attention --bidirectional-rnn $FASTTEXT $FTMODELARG --fasttext-model-size 300 --max-training-minutes 4300 --merge-subwords $CHARACTEREMBEDDINGS --replace-all-numbers --pretrained-model $PRETRAINEDMODEL
+srun python3 -u run.py --device cuda --eval-freq 100 --seed 13 --patience 25 --epochs 500 --configuration rnn-simple --learning-rate $LEARNINGRATE --metric-types f1-score precision-score recall-score --language $LANGUAGE --challenge named-entity-recognition --batch-size 128 --enable-external-logging --pretrained-weights $PRETRAINEDWEIGHTS --hidden-dimension $HIDDENSIZE --embeddings-size $EMBEDDINGSSIZE --dropout $DROPOUT --number-of-layers $NUMBERLAYERS $ENTITYTAGTYPES --reset-training-on-early-stop --training-reset-epoch-limit 5 $INCLUDEPRETR --pretrained-model-size 768 --pretrained-max-length 512 --learn-new-embeddings --checkpoint-name $CHECKPOINTNAME --no-attention $BIDIRECTIONAL $FASTTEXT $FTMODELARG --fasttext-model-size 300 --max-training-minutes 760 --merge-subwords $CHARACTEREMBEDDINGS --replace-all-numbers --pretrained-model $PRETRAINEDMODEL
 
 cp -a "$TMPDIR"/eval-historical-texts/wandb/ $HOME/eval-historical-texts/
 cp -a "$TMPDIR"/eval-historical-texts/results/ $HOME/eval-historical-texts/
