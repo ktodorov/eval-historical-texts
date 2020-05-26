597f91cc3b475f64ab44d5840a0d798a901dfda8
ktodorov <kztodorov@outlook.com>
2020-05-26 10:59:34 +0200

    Refactors existing jobs

--- jobs/train_ner.sh
+++ jobs/train_ner.sh
@@ -3,26 +3,25 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=3
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=13:00:00
+#SBATCH --time=25:00:00
 #SBATCH --mem=60000M
 #SBATCH -p gpu_shared
-#SBATCH --gres=gpu:1
 
 module purge
 module load pre2019
 module load 2019
 module load Miniconda3
-module load Python/3.7.5-foss-2018b
+module load Python/3.7.5-foss-2019b
 
 source activate eval-env
 
-echo copying to SCRATCH...
-shopt -s extglob # This is to enable ! on the next line
-mkdir -p "$TMPDIR"/eval-historical-texts/results
-cp -a $HOME/eval-historical-texts/!(results) "$TMPDIR"/eval-historical-texts
-echo copying finished
+# echo copying to SCRATCH...
+# shopt -s extglob # This is to enable ! on the next line
+# mkdir -p "$TMPDIR"/eval-historical-texts/results
+# cp -a $HOME/eval-historical-texts/!(results) "$TMPDIR"/eval-historical-texts
+# echo copying finished
 
-cd "$TMPDIR"/eval-historical-texts
+# cd "$TMPDIR"/eval-historical-texts
 
 FT=""
 FTMODELARG=""
@@ -198,7 +197,7 @@ then
     BATCHSIZE="128"
 fi
 
-srun python3 -u run.py --device cuda --eval-freq $EVALFREQ --seed 13 --patience $PATIENCEARG --epochs 500 --configuration rnn-simple --learning-rate $LEARNINGRATE --metric-types f1-score precision recall --language $LANGUAGE --challenge named-entity-recognition --batch-size $BATCHSIZE --enable-external-logging --pretrained-weights $PRETRAINEDWEIGHTS --hidden-dimension $HIDDENSIZE --embeddings-size $EMBEDDINGSSIZE --dropout $DROPOUT --number-of-layers $NUMBERLAYERS $ENTITYTAGTYPES --reset-training-on-early-stop --training-reset-epoch-limit 5 $INCLUDEPRETR --pretrained-model-size 768 --pretrained-max-length 512 $NEWEMBEDDINGSARG --checkpoint-name $CHECKPOINTNAME --no-attention $BIDIRECTIONAL $FASTTEXT $FTMODELARG --fasttext-model-size 300 --max-training-minutes 760 --merge-subwords $CHARACTEREMBEDDINGS --replace-all-numbers --pretrained-model $PRETRAINEDMODEL $FINETUNEARG $WEIGHTEDLOSSARG $MANUALFEATURESARG $SPLITARG
+python run.py --device cuda --eval-freq $EVALFREQ --seed 13 --patience $PATIENCEARG --epochs 500 --configuration rnn-simple --learning-rate $LEARNINGRATE --metric-types f1-score precision recall --language $LANGUAGE --challenge named-entity-recognition --batch-size $BATCHSIZE --enable-external-logging --pretrained-weights $PRETRAINEDWEIGHTS --hidden-dimension $HIDDENSIZE --embeddings-size $EMBEDDINGSSIZE --dropout $DROPOUT --number-of-layers $NUMBERLAYERS $ENTITYTAGTYPES --reset-training-on-early-stop --training-reset-epoch-limit 5 $INCLUDEPRETR --pretrained-model-size 768 --pretrained-max-length 512 $NEWEMBEDDINGSARG --checkpoint-name $CHECKPOINTNAME --no-attention $BIDIRECTIONAL $FASTTEXT $FTMODELARG --fasttext-model-size 300 --max-training-minutes 1440 --merge-subwords $CHARACTEREMBEDDINGS --replace-all-numbers --pretrained-model $PRETRAINEDMODEL $FINETUNEARG $WEIGHTEDLOSSARG $MANUALFEATURESARG $SPLITARG
 
-cp -a "$TMPDIR"/eval-historical-texts/wandb/ $HOME/eval-historical-texts/
-cp -a "$TMPDIR"/eval-historical-texts/results/ $HOME/eval-historical-texts/
+# cp -a "$TMPDIR"/eval-historical-texts/wandb/ $HOME/eval-historical-texts/
+# cp -a "$TMPDIR"/eval-historical-texts/results/ $HOME/eval-historical-texts/
