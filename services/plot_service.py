580907bca2f686db577451a57c23a335ce3c4a34
ktodorov <kztodorov@outlook.com>
2020-07-16 16:45:28 +0200

    Adds more plot types to plot service

--- services/plot_service.py
+++ services/plot_service.py
@@ -17,6 +17,7 @@ class PlotService:
             data_service: DataService):
         sns.set()
         sns.set_style("ticks")
+        sns.set(font_scale=1.5)  # crazy big
 
         self._data_service = data_service
 
@@ -67,6 +68,21 @@ class PlotService:
 
         plt.clf()
 
+        return ax
+
+    def autolabel_heights(self, ax, rects):
+        """Attach a text label above each bar in *rects*, displaying its height."""
+        for rect in rects:
+            height = rect.get_height()
+            if height == 0:
+                continue
+
+            ax.annotate('{}'.format(height),
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
+
     def plot_counters_histogram(
             self,
             counter_labels: List[str],
@@ -78,6 +94,7 @@ class PlotService:
             filename: str = None,
             xlabel: str = None,
             ylabel: str = None,
+            plot_values_above_bars: bool = False,
             ax=None,
             hide_axis: bool = False):
 
@@ -89,7 +106,7 @@ class PlotService:
 
         values = []
         for counter in counters:
-            values.append([counter[label] for label in unique_labels])
+            values.append([(counter[label] if label in counter.keys() else 0) for label in unique_labels])
 
         total_width = 0.8  # the width of the bars
         dim = len(counters)
@@ -103,7 +120,7 @@ class PlotService:
         rects = []
         for i, counter_values in enumerate(values):
             rects.append(
-                [ax.bar(x + (i * dimw), counter_values, dimw, label=counter_labels[i], color=counter_colors[i])])
+                ax.bar(x + (i * dimw), counter_values, dimw, label=counter_labels[i], color=counter_colors[i]))
 
         ax.set_xticks(x + (total_width - dimw) / 2)
         ax.set_xticklabels(unique_labels)
@@ -114,6 +131,12 @@ class PlotService:
 
         if xlabel is not None:
             ax.set_xlabel(xlabel)
+
+        if plot_values_above_bars:
+            for rect in rects:
+                self.autolabel_heights(ax, rect)
+                x1,x2,y1,y2 = ax.axis()
+                ax.axis((x1,x2,y1,y2 + 5))
 
         self._add_properties(
             ax,
@@ -127,6 +150,8 @@ class PlotService:
             plt.show()
 
         plt.clf()
+
+        return ax
 
     def plot_scatter(
             self,
@@ -158,6 +183,8 @@ class PlotService:
 
         if show_plot or (save_path is not None and filename is not None):
             plt.clf()
+
+        return ax
 
     def plot_labels(
             self,
@@ -198,6 +225,8 @@ class PlotService:
         if show_plot or (save_path is not None and filename is not None):
             plt.clf()
 
+        return ax
+
     def plot_arrow(
             self,
             x: float,
@@ -232,6 +261,8 @@ class PlotService:
 
         if show_plot or (save_path is not None and filename is not None):
             plt.clf()
+
+        return ax
 
     def plot_confusion_matrix(
             self,
@@ -290,6 +321,73 @@ class PlotService:
 
         if show_plot or (save_path is not None and filename is not None):
             plt.clf()
+
+        return ax
+
+
+    def plot_heatmap(
+            self,
+            values: np.array,
+            labels: List[str] = None,
+            title: str = None,
+            title_padding: float = None,
+            vmin: float = None,
+            vmax: float = None,
+            y_title: str = None,
+            x_title: str = None,
+            show_colorbar: bool = True,
+            save_path: str = None,
+            filename: str = None,
+            ax=None,
+            show_plot: bool = True,
+            hide_axis: bool = False):
+        if ax is None:
+            ax = self.create_plot()
+
+        if vmin is None:
+            vmin = np.min(values)
+
+        if vmax is None:
+            vmax = np.max(values)
+
+        sns_heatmap = sns.heatmap(
+            values,
+            ax=ax,
+            vmin=vmin,
+            vmax=vmax,
+            cmap='Greens',
+            square=True,
+            cbar=show_colorbar)
+
+        if x_title is not None:
+            ax.set_xlabel(x_title)
+
+        if y_title is not None:
+            ax.set_ylabel(y_title)
+
+        if labels is not None:
+            ax.set_ylim(0, len(labels) + 0.5)
+            ax.set_ylim(0, len(labels) + 0.5)
+
+            sns_heatmap.set_yticklabels(labels, rotation=0)
+            sns_heatmap.set_xticklabels(
+                labels, rotation=45, horizontalalignment='right')
+
+        self._add_properties(
+            ax,
+            title,
+            title_padding,
+            save_path,
+            filename,
+            hide_axis)
+
+        if show_plot and (save_path is None or filename is None):
+            plt.show()
+
+        if show_plot or (save_path is not None and filename is not None):
+            plt.clf()
+
+        return ax
 
     def _add_properties(
             self,
